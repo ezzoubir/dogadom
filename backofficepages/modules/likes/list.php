5a2e6c26fe3b605b5eadc9b0ef30d1c4e3923745
ezzoubir <douiderezzoubir@gmail.com>
2015-03-06 14:49:18 +0000

    zoubir

--- backofficepages/modules/likes/list.php
+++ backofficepages/modules/likes/list.php
@@ -1,15 +1,10 @@
 <?php
-	if(isset($_POST['delete']))
-	{
-	   $sql='update paiements set deleted = 1 where id='.GetImageButtonValue($_POST['delete']);
-	   mysql_query($sql);
 
-	}
-	$sql=mysql_query('select * from paiements where deleted = 0 order by id desc');
+	$sql=mysql_query('select * from shares order by id desc');
 ?>
 <div class="row">
                 <div class="col-lg-12">
-                    <h1 class="page-header">Paiements</h1>
+                    <h1 class="page-header">Shares</h1>
                 </div>
                 <!-- /.col-lg-12 -->
             </div>
@@ -18,37 +13,37 @@
                 <div class="col-lg-12">
                     <div class="panel panel-default">
                         <div class="panel-heading">
-                            Liste des paiements
+                            Liste des shares
                         </div>
                         <!-- /.panel-heading -->
                         <div class="panel-body">
                             <div class="table-responsive">
-                            <form role="form" action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="post" enctype="multipart/form-data">
                                 <table class="table table-striped table-bordered table-hover" id="dataTables-example">
                                     <thead>
                                         <tr>
-                                            <th>Membre</th>
-                                            <th>Total</th>
-                                            <th>Date</th>
-                                            <th>Actions</th>
+                                            <th>Date Share</th>
+                                            <th>Company</th>
+                                            <th>Title</th>
+                                            <th>Share User</th>
+                                            <th>Paye</th>
                                         </tr>
                                     </thead>
                                     <tbody>
 									<?php
 										while($data=mysql_fetch_array($sql)){
 									?>
-                                        <tr class="odd gradeX">
-											<td><?php echo getUserNameByIdfb($data['user_id']); ?></td>
-                                            <td><?php echo $data['total']; ?> Dhs</td>
+                                        <tr class="odd gradeX" <?php if($data['paye']==1) { echo 'style="background:green"';} else { echo 'style="background:red"'; } ?>>
 											<td><?php echo $data['created']; ?></td>
-											<td><input type="image" name="delete[<?php echo $data['id']; ?>]" src="imgs/delete.png" onclick='javascript: if(confirm("etes-vous certain de supprimer ce paiement ?")){this.submit();} else return false;'></td>
+											<td><?php echo getCompanyNameByAd($data['ad_id']); ?></td>
+											<td><?php echo $data['title']; ?></td>
+											<td><?php echo getUserNameByAd($data['user_id']); ?></td>
+											<td><?php echo $data['paye']; ?></td>
                                         </tr>
 									<?php
 										}
 									?>
                                     </tbody>
                                 </table>
-                            </form>
                             </div>
                             <!-- /.table-responsive -->
                         </div>
